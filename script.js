f9a48e429e55e16400c95e40713963fe0f70ac07
ndrada <andrada.groseanu@gmail.com>
2023-09-05 06:27:38 -0700

    simple-quote-generator

---
+++ script.js
@@ -0,0 +1,17 @@
+function generate(){
+    let quotes = {
+        "-Jules Renard" : '"The truly free man is the one who can turn down an invitation to dinner without giving an excuse."',
+        "-Mary Astell" : '"If all men are born free, how is it that all women are born slaves?"',
+        "-Virginia Woolf" :'"The history of men’s opposition to women’s emancipation is more interesting perhaps than the story of that emancipation itself."',
+        "-Jaclyn Friedman" : "'One of the best ways to free yourself to what you want is to feel secure in your ability to say no to what you don’t.'"
+    }
+
+    const authors = Object.keys(quotes);
+
+    const source = authors[Math.floor(Math.random() * authors.length)];
+
+    const cite = quotes[source];
+
+    document.getElementById('quote').innerHTML = cite;
+    document.getElementById('author').innerHTML = source;
+}
